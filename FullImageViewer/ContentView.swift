3f96061e81e40dce447447c4b6a883417c7f37e1
楊宗翰 <feveraly@gmail.com>
2020-12-22 13:52:57 +0800

    Initial Commit

---
+++ FullImageViewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FullImageViewer
+//
+//  Created by 楊宗翰 on 2020/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
